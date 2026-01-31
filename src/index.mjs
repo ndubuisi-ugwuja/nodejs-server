f5b744b1fa7b4a78ec1d0531ddafe88362860a76
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-01-31 04:04:34 +0100

    update index

--- src/index.mjs
+++ src/index.mjs
@@ -5,8 +5,19 @@ const app = express()
 
 const {PORT} = process.env || 3000
 
+const mockUsers = [{id: 1, name: "Ndubuisi"}, {id: 2, name: "Jiovta"}, {id: 1, name: "Ugwuja"},]
+
 app.get("/", (request, response) => {
-    response.send("Hi, I got the request")
+    response.status(201).send({msg: "Got it"})
+})
+
+app.get("/api/users", (request, response) => {
+    response.status(201).send(mockUsers)
+})
+
+app.get("/api/users/:id", (request, response) => {
+    console.log(request.params)
+    response.sendStatus(200)
 })
 
 app.listen(PORT, () => {
