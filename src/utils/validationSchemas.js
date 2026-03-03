a8386f387da3dad3206e7792e80a9ea833c0c11a
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-03-03 15:03:19 +0100

    add full username validation schema

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,11 +1,14 @@
 export const createUserValidationSchema = {
     username: {
+        notEmpty: {
+            msg: "Username cannot be empty"
+        },
+        isString: {
+            msg: "Username must be a string"
+        },
         isLength: {
             max: 10,
             min: 3
-        },
-        notEmpty: {
-            msg: "Username cannot be empty"
         }
     }
 }
