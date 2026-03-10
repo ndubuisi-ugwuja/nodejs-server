5b29eb9c8c6f3e0f3601d427de7792dfd395e728
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-03-10 17:54:43 +0100

    remove route folder

--- src/routes/users.mjs
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router()
-
-router.get("/api/user", (request, response) => {
-    console.log(request.params)
-
-    response.status(200).send({msg: "This is the root directory from router"})
-})
