1d457971200a106b4bc44b47d12f072884a12028
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-03-16 10:49:55 +0100

    update

--- src/index.mjs
+++ src/index.mjs
@@ -16,7 +16,7 @@ mongoose.connect("mongodb://localhost/express-backend")
     .catch((err) => console.log("Error:", err))
 
 
-// Middleware
+// Middlewares
 const resolveIndexByUserId = (request, response, next) => {
     const {params: {id}} = request
 
