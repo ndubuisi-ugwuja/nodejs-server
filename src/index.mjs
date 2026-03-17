25343d57d3ed12b64e72ba0e23b5d4b4700b623a
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-03-17 08:00:50 +0100

    update session

--- src/index.mjs
+++ src/index.mjs
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(session());
+app.use(session())
 
 mongoose.connect("mongodb://localhost/express-backend")
     .then(() => console.log("Connected to Database"))
