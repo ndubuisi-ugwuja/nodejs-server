8174f63237882d43d395b2527260c61ffdc8a70a
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-03-17 08:14:00 +0100

    fix syntax error

--- src/index.mjs
+++ src/index.mjs
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(session({
-    secrete: "dxx13q"
+    secret: "dxx13q"
 }))
 
 mongoose.connect("mongodb://localhost/express-backend")
