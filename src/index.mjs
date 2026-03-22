c480d55b95e557aff44fd927bbdeb116e85476c4
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-03-22 16:52:14 +0100

    update api

--- src/index.mjs
+++ src/index.mjs
@@ -49,6 +49,7 @@ app.get("/", (request, response) => {
     request.session.visited = true
     console.log(request.session)
     console.log(request.session.id)
+    request.sectionStore.get()
     response.cookie("Test cookies", "base url cookie", {maxAge: 60000 * 60}) // expires in 1min (unit in milliseconds)
     response.status(200).send({msg: "This is the root directory"})
 })
