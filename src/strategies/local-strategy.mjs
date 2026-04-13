99b688d6ccfdb7b9b6c26d6ba6dc2760b9a3b455
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-04-13 06:39:51 +0100

    update strategy

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -5,6 +5,8 @@ passport.serializeUser((user, done) => {
     done(null, user.id)
 })
 
+passport.deserializeUser()
+
 const mockUsers = [{id: 1, name: "Ndubuisi", username: "Ndu123", password: "123"}, {id: 2, name: "Jiovta", username: "Jio123", password: "1234"}, {id: 3, name: "Ugwuja", username: "Ugw123", password: "12345"},]
 
 export default passport.use(
