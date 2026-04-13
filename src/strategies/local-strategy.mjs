aad53ba98db421a14ac6323f237afac4ae590cc7
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-04-13 06:11:01 +0100

    update local strategy

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -3,7 +3,7 @@ import { Strategy } from "passport-local";
 
 const mockUsers = [{id: 1, name: "Ndubuisi", username: "Ndu123", password: "123"}, {id: 2, name: "Jiovta", username: "Jio123", password: "1234"}, {id: 3, name: "Ugwuja", username: "Ugw123", password: "12345"},]
 
-passport.use(
+export default passport.use(
     new Strategy((username, password, done) => {
         try {
             const findUser = mockUsers.find((user) => user.username === username)
