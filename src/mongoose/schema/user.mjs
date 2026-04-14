fbbefd5ed9d4e3ed98d272ba270581ea3219971a
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-04-14 06:59:11 +0100

    update schema

--- src/mongoose/schema/user.mjs
+++ src/mongoose/schema/user.mjs
@@ -1,14 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-    name: {
-        type: mongoose.Schema.Types.String,
-        required: true
-    },
     username: {
         type: mongoose.Schema.Types.String,
         required: true,
         unique: true
+    },
+    password: {
+        type: mongoose.Schema.Types.String,
+        required: true
     }
 })
 
