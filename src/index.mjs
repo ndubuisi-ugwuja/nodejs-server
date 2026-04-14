67a02067ec6e9d349d67a46fd81ef4aa9f918807
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-04-14 07:23:38 +0100

    update index js

--- src/index.mjs
+++ src/index.mjs
@@ -170,29 +170,6 @@ app.post("/api/auth/logout", (request, response) => {
     })
 })
 
-// Simple virtual cart system
-app.post("/api/cart", (request, response) => {
-    if(!request.session.user) return response.sendStatus(401)
-
-    const {body: item} = request
-
-    const {cart} = request.session
-
-    if(cart) {
-        cart.push(item)
-    } else {
-        request.session.cart = [item]
-    }
-
-    return response.status(201).send(item)
-})
-
-app.get("/api/cart", (request, response) => {
-    if(!request.session.user) return response.sendStatus(401)
-
-    return response.status(200).send(request.session.cart ?? [])
-})
-
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
