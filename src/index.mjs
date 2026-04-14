ddae1dd2dddc0575ee2c6fca229338845b99c2d3
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-04-14 07:49:12 +0100

    update index js

--- src/index.mjs
+++ src/index.mjs
@@ -48,8 +48,6 @@ const resolveIndexByUserId = (request, response, next) => {
 
 const {PORT} = process.env || 3000
 
-const mockUsers = [{id: 1, name: "Ndubuisi", username: "Ndu123", password: "123"}, {id: 2, name: "Jiovta", username: "Jio123", password: "1234"}, {id: 3, name: "Ugwuja", username: "Ugw123", password: "12345"},]
-
 app.get("/", (request, response) => {
     request.session.visited = true
     console.log(request.session)
