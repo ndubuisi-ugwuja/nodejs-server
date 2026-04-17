1b49acf2edea8a4e6eb70bdca0ad9332b142fda7
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-04-17 23:20:08 +0100

    fix serialization bug

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -3,21 +3,6 @@ import { Strategy } from "passport-local";
 import { User } from "../mongoose/schemas/user.mjs";
 import { comparePassword } from "../utils/helpers.mjs";
 
-passport.serializeUser((user, done) => {
-    done(null, user.id)
-})
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const findUser = await User.findById(id)
-        if(!findUser) throw new Error("Bad credentials")  
-                
-        done(null, findUser)
-    } catch(err) {
-        done(err, null)
-    }
-})
-
 export default passport.use(
     new Strategy(async (username, password, done) => {
         try {
